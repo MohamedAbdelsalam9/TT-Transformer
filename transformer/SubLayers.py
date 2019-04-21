5647c32bcd7b232efb172a34ef6e13cd66c00266
MohamedAbdelsalam9 <mohamedashraf9412@gmail.com>
2019-04-21 13:56:07 -0400

    Rewrite PositionwiseFeedForward as a nn.Linear instead of nn.Conv1d

--- transformer/SubLayers.py
+++ transformer/SubLayers.py
@@ -66,16 +66,15 @@ class PositionwiseFeedForward(nn.Module):
 
     def __init__(self, d_in, d_hid, dropout=0.1):
         super().__init__()
-        self.w_1 = nn.Conv1d(d_in, d_hid, 1) # position-wise
-        self.w_2 = nn.Conv1d(d_hid, d_in, 1) # position-wise
+        self.w_1 = nn.Linear(d_in, d_hid)
+        self.w_2 = nn.Linear(d_hid, d_in)
         self.layer_norm = nn.LayerNorm(d_in)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         residual = x
-        output = x.transpose(1, 2)
+        output = x
         output = self.w_2(F.relu(self.w_1(output)))
-        output = output.transpose(1, 2)
         output = self.dropout(output)
         output = self.layer_norm(output + residual)
         return output
